914d585b1b23cdf4a5c140f0bb975c74b7021274
Lerrika Winchester <ilerrika@gmail.com>
2017-09-18 13:51:28 +0300

    bot and site

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
-console.log("my bot app");
-process.title = "MyTestBot"
-process.on('uncaughtException', function(error) {
-    log.add('Упс, произошла непредвиденная ошибка: '+error.stack);
-    console.error(error.stack);
-    return false;
+var startBot = require('./bot.js')
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
-var TelegramBot = require('node-telegram-bot-api');
-var tg;
-function create() {
-    var token = "387016243:AAEXimznXpHl5ke6qpUanexj_Wm9mH79y_s";
-    tg = new TelegramBot(token, {
-        polling: true
-    });
-    tg.on('message', onMessage);
-    tg.on('callback_query', onCallbackQuery);
-}
-function onMessage(message) {
-    console.log('message:', message);
-    if (message.text && message.text.toLowerCase() == 'ping') {
-        tg.sendMessage(message.chat.id, '<pre>pong</pre>', {
-            parse_mode:'HTML'
-        });
-        return;
-    }
-    //
-    if (message.text && message.text.toLowerCase() == '/start') {
-        sendStartMessage(message);
-        return;
-    }
-}
-function onCallbackQuery(callbackQuery) {
-    console.log('callbackQuery:', callbackQuery);
-    if (callbackQuery.data == 'helpCmd') {
-        var helpText = "какой то текст об игре";
-        tg.sendMessage(callbackQuery.message.chat.id, helpText);
-        tg.answerCallbackQuery(callbackQuery.id);
-    } else if (callbackQuery.data == 'gameCmd') {
-        // стартуем нашу игру
-    }
-}
-// *********************************************
-function sendStartMessage(message) {
-    var text = 'Добро пожаловать в нашу супер-пупер игру';
-    //
-    var helpButton = {
-        text:"Об игре",
-        callback_data:'helpCmd'
-    }
-    //
-    var gameButton = {
-        text:"Начать игру",
-        callback_data:'gameCmd'
-    }
-    //
-    var options = {};
-    options.reply_markup = {};
-    options.reply_markup.inline_keyboard = [];
-    options.reply_markup.inline_keyboard.push([helpButton]);
-    options.reply_markup.inline_keyboard.push([gameButton]);
-    tg.sendMessage(message.chat.id, text, options);
-}
-create();
+
+app.get('/api/telegram', function(request, response) {
+    
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+startBot()
